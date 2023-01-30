0ac17e92118ae86c224cbce71493edb058719239
sinnieth <sinneith@gmail.com>
2023-01-30 15:59:57 +0900

    edit pop_assign script

--- static/js/open_pop_assign.js
+++ static/js/open_pop_assign.js
@@ -2,11 +2,7 @@ const timeList = document.querySelectorAll(".reservation_list > ul > li");
 
 timeList.forEach((time) => {
   time.addEventListener("click", () => {
-    if (time.classList.contains("clicked")) {
-      time.classList.remove("clicked");
-    } else {
-      time.classList.add("clicked");
-      let openWin = window.open("pop_assign.html");
-    }
+    time.classList.add("clicked");
+    let openWin = window.open("pop_assign.html");
   });
 });
--- static/js/pop_assign.js
+++ static/js/pop_assign.js
@@ -2,15 +2,17 @@ const therapistList = document.querySelectorAll("li");
 const clickedTime = window.opener.document.querySelector(
   ".clicked > a > .name"
 );
+const clickedTimeClass = window.opener.document.querySelector(".clicked");
 
 therapistList.forEach((li) => {
   li.addEventListener("click", () => {
     const therapist = li.querySelector(".name span").innerText;
     sendValue(therapist);
+    clickedTimeClass.classList.remove("clicked");
+    window.close();
   });
 });
 
 function sendValue(therapist) {
   clickedTime.innerText = therapist;
-  window.close();
 }
