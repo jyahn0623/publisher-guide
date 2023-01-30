b3d574881acc533fa70e868fb2f01d26ddb8eb57
sinnieth <sinneith@gmail.com>
2023-01-30 15:39:34 +0900

    add pop_assign part script

--- static/js/pop_assign.js
+++ static/js/pop_assign.js
@@ -1,21 +1,16 @@
-/* 자식창 */
-const therapistArr = document.querySelectorAll(".therapist_list > ul > li");
-/* 부모창에서 보내준 아이디값, 식별 정보 담을 그릇 */
-const id = document.querySelector(".temp");
+const therapistList = document.querySelectorAll("li");
+const clickedTime = window.opener.document.querySelector(
+  ".clicked > a > .name"
+);
 
-therapistArr.forEach((li) => {
+therapistList.forEach((li) => {
   li.addEventListener("click", () => {
-    let name = li.querySelector(".name > span");
-
-    /*     자식창의 값이 들어갈 부모창의 요소 = 자식창에서 전달할 값 */
-    /* opener.document.name.value = document.getE; */
-
-    window.close();
+    const therapist = li.querySelector(".name span").innerText;
+    sendValue(therapist);
   });
 });
-console.dir(id);
 
-/* 
-부모창에서 또 정보를 보내줘야 하네. 
-지금 클릭한 게 어떤 리스트인지.
- */
+function sendValue(therapist) {
+  clickedTime.innerText = therapist;
+  window.close();
+}
